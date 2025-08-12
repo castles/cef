0ad3b23eccf5655f9d659b5c32926b242caa3e30
Marc <marc@launchinteractive.com.au>
2025-08-12 12:13:54 +1000

    change remove_directory function call to delete_directory

--- tools/automate/automate-git.py
+++ tools/automate/automate-git.py
@@ -77,7 +77,7 @@ def copy_directory(source, target, allow_overwrite=False):
   if not options.dryrun and os.path.exists(target):
     if not allow_overwrite:
       raise Exception("Directory %s already exists" % (target))
-    remove_directory(target)
+    delete_directory(target)
   if os.path.exists(source):
     msg("Copying directory %s to %s" % (source, target));
     if not options.dryrun:
@@ -88,7 +88,7 @@ def move_directory(source, target, allow_overwrite=False):
   if not options.dryrun and os.path.exists(target):
     if not allow_overwrite:
       raise Exception("Directory %s already exists" % (target))
-    remove_directory(target)
+    delete_directory(target)
   if os.path.exists(source):
     msg("Moving directory %s to %s" % (source, target));
     if not options.dryrun:
